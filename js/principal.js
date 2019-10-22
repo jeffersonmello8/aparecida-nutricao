f0953d19e12c5326d85c228221740d321d42b9a1
jefferson <jefferson.mello8@hotmail.com>
2019-10-22 20:50:50 -0300

    coloquei para calcular o imc de todos os pacientes pela classe css

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i ++) {
+    
+    paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent; 
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 200) {
+        console.log("O peso do paciente é inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = ("Peso inválido!")
+    } 
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("A altura do paciente é inválida");
+        alturaEhValida = false;
+        tdImc.textContent = ("Altura inválida!")
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
