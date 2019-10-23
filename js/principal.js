cb8d32b9f385274da83f894ca2afe38cc57ffcdb
jefferson <jefferson.mello8@hotmail.com>
2019-10-22 21:05:31 -0300

    adicionei uma coloração a linha do paciente que tiver alguma informação inválida para sinalizar melhor ao usuário

--- js/principal.js
+++ js/principal.js
@@ -22,12 +22,14 @@ for (var i = 0; i < pacientes.length; i ++) {
         console.log("O peso do paciente é inválido!");
         pesoEhValido = false;
         tdImc.textContent = ("Peso inválido!")
+        paciente.classList.add("paciente-invalido")
     } 
 
     if (altura <= 0 || altura >= 3.00) {
         console.log("A altura do paciente é inválida");
         alturaEhValida = false;
         tdImc.textContent = ("Altura inválida!")
+        paciente.classList.add("paciente-invalido")
     }
 
     if (pesoEhValido && alturaEhValida) {
